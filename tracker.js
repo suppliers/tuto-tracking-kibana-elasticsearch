689fb9efd0baf94789cb637a05fdb565a1e03fed
Nicolas RAMY <nicolas.ramy@gmail.com>
2013-09-03 03:23:06 +0200

    Add tags parsing

--- tracker.js
+++ tracker.js
@@ -1,12 +1,22 @@
 function tracker() {
-  data = {"track": 
+  // Check for anchor
+  tags_slug = location.href.split("#");
+  if (tags_slug.length > 1) {
+    // Make an array in spliting
+    tags = tags_slug[1].split('-');
+  } else {
+    tags = new Array();
+  }
+
+
+  data = {"track":
           {
             "userAgent": navigator.userAgent,
             "platform": navigator.platform,
             "host": location.host,
             "currentPage": location.href,
             "previousPage": location.origin,
-            "url": location.pathname
+            "tags": tags
           }
          };
   return data;
