33ee37d4e6c2d7714149df65c0d24f97768f3772
Nicolas RAMY-SEPOU <nicolas.ramy-sepou@mediapart.fr>
2013-09-13 18:47:50 +0200

    Fix cURL submission

--- api/index.php
+++ api/index.php
@@ -1,13 +1,20 @@
 <?php
 if (isset($_SERVER['HTTP_REFERER'], $_GET['data'])) {
+
     // Initialize cURL
     $cURL = curl_init();
 
+    $obj_data = json_decode($_GET['data']);
+    $obj_data->created = date('r');
+    $data = json_encode($obj_data);
+
+    //file_put_contents('log/data', $data . PHP_EOL, FILE_APPEND);
+
     // Configure cURL options
     $options = array(
-        CURLOPT_URL => 'http://127.0.0.1:9200/kibana-website-tracker/tracl/',
+        CURLOPT_URL => 'http://127.0.0.1:9200/tracker/hit/' . uniqid('', true),
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_CUSTOMREQUEST => 'PUT',
         CURLOPT_POST => 1,
         CURLOPT_POSTFIELDS => $data
     );
@@ -17,6 +24,8 @@ if (isset($_SERVER['HTTP_REFERER'], $_GET['data'])) {
 
     // Get the results
     $result = curl_exec($cURL);
+
+    //file_put_contents('log/api', $result . PHP_EOL, FILE_APPEND);
 
     // Close resource
     curl_close($cURL);
